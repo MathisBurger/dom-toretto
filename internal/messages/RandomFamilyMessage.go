15ee7dfc43b94e23f25d9ac0f0ded48b7127780e
Mathis Burger <handymathis@gmail.com>
2024-04-21 22:30:06 +0200

    [TASK] Added some more family messages

--- internal/messages/RandomFamilyMessage.go
+++ internal/messages/RandomFamilyMessage.go
@@ -11,6 +11,17 @@ var FamilyMessages = []string{
 	"Did you say family?",
 	"I would kill for my family!",
 	"I have no friends, I have family!",
+	"My family is everything",
+	"I would learn Java for my family",
+	"Nothing over a corona beer",
+	"My team is my family",
+	"CSGO team is family",
+	"I rush B with my family",
+	"I rush A with my family",
+	"My family is more important than anything else",
+	"I only shit on family toilets",
+	"Only people who drive Audi are family",
+	"My family is the best thing I know",
 }
 
 func RandomFamilyMessage() string {
